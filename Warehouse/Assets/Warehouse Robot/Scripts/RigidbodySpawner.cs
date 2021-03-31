ead6277d3d90d7d1fe8b5367415e995acff96430
Devin Miller (Unity) <devin.miller@unity3d.com>
2021-03-31 15:14:04 -0700

    Merging in Vidur's branch

--- Warehouse/Assets/Warehouse Robot/Scripts/RigidbodySpawner.cs
+++ Warehouse/Assets/Warehouse Robot/Scripts/RigidbodySpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,113 +6,161 @@ using UnityEngine.UI;
 using UnityEngine.Perception.GroundTruth;
 using RosSharp;
 
-namespace Unity.Simulation.Warehouse {
-    public class RigidbodySpawner : MonoBehaviour
+public class RigidbodySpawner : MonoBehaviour
+{
+    public GameObject LocationPicker;
+    public GameObject Robot;
+    public GameObject m_box;
+    private Vector3 m_boxDims;
+    private int m_debrisSpawn;
+    private GameObject m_spawnedBoxes;
+    private static bool RobotInitiated = false;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        public GameObject locationPicker;
-        public GameObject box;
-        private Vector3 boxDims;
-        private int debrisSpawn;
-        private GameObject spawned;
-        private Bounds pickerArea;
+        HidePicker();
+    }
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            ShowPicker(false);
+    public void SpawnBoxes(){
+        var boxIn = transform.Find("SpawnBoxes").GetComponent<InputField>().text.Split(',');
+        if (boxIn.Length != 3){
+            Debug.LogError($"Invalid box input dimensions!");
+            return;
         }
 
-        public void SpawnBoxes()
-        {
-            pickerArea = locationPicker.GetComponent<Renderer>().bounds;
-            var boxIn = transform.Find("SpawnBoxes").GetComponent<InputField>().text.Split(',');
-            if (boxIn.Length != 3){
-                Debug.LogError($"Invalid box input dimensions!");
-                return;
-            }
-
-            if (spawned != null) 
-            {
-                Destroy(spawned);
-            }
-                
-            spawned = new GameObject("Spawned");
-            boxDims = new Vector3(int.Parse(boxIn[0]), int.Parse(boxIn[1]), int.Parse(boxIn[2]));
+        if (m_spawnedBoxes != null) {
+            Destroy(m_spawnedBoxes);
+        }
             
-            var boxSize = box.GetComponentInChildren<Renderer>().bounds.size;
-            for (int i = 0; i < boxDims[0]; i++)
-            {
-                for (int j = 0; j < boxDims[1]; j++)
-                {
-                    for (int k = 0; k < boxDims[2]; k++)
-                    {
-                        var o = Instantiate(box, new Vector3(i * boxSize.x, k * boxSize.y, j * boxSize.z), Quaternion.identity, spawned.transform);
-                        Destroy(o.GetComponent<BoxDropoff>());
-                    }
+        m_spawnedBoxes = new GameObject("Spawned");
+        m_boxDims = new Vector3(int.Parse(boxIn[0]), int.Parse(boxIn[1]), int.Parse(boxIn[2]));
+        
+        var boxSize = m_box.GetComponentInChildren<Renderer>().bounds.size;
+        for (int i = 0; i < m_boxDims[0]; i++){
+            for (int j = 0; j < m_boxDims[1]; j++){
+                for (int k = 0; k < m_boxDims[2]; k++){
+                    var o = Instantiate(m_box, new Vector3(i * boxSize.x, k * boxSize.y, j * boxSize.z), Quaternion.identity, m_spawnedBoxes.transform);
+                    Destroy(o.GetComponent<BoxDropoff>());
                 }
             }
-
-            spawned.transform.position = locationPicker.transform.position;
         }
 
-        public void SpawnDebris()
-        {
-            pickerArea = locationPicker.GetComponent<Renderer>().bounds;
-            debrisSpawn = int.Parse(transform.Find("SpawnDebris").GetComponent<InputField>().text);
+        m_spawnedBoxes.transform.position = LocationPicker.transform.position;
+    }
 
-            if (spawned != null) 
-            {
-                Destroy(spawned);
-            }
+    public void SpawnDebris(){
+        m_debrisSpawn = int.Parse(transform.Find("SpawnDebris").GetComponent<InputField>().text);
+
+        if (m_spawnedBoxes != null) {
+            Destroy(m_spawnedBoxes);
+        }
+            
+        m_spawnedBoxes = new GameObject("Spawned");
+
+        var size = GameObject.FindObjectOfType<WarehouseManager>().GetEditorParams().m_debrisSize;
+
+        for (int i = 0; i < m_debrisSpawn; i++){
+            var randShape = UnityEngine.Random.Range(0, 4);
+            GameObject obj;
+            switch(randShape){
+                case 0:
+                    obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    break;
+                case 1:
+                    obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    break;
+                case 2:
+                    obj = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                    break;
+                case 3:
+                    obj = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+                    break;
+                default:
+                    obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    break;
                 
-            spawned = new GameObject("Spawned");
-
-            var size = WarehouseManager.instance.m_debrisSize;
-            var isKinematic = WarehouseManager.instance.m_debrisKinematic;
-
-            for (int i = 0; i < debrisSpawn; i++)
-            {
-                var randShape = Random.Range(0, 4);
-                GameObject obj;
-                switch(randShape){
-                    case 0:
-                        obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                        break;
-                    case 1:
-                        obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                        break;
-                    case 2:
-                        obj = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-                        break;
-                    case 3:
-                        obj = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-                        break;
-                    default:
-                        obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                        break;
-                    
-                }
-                var pos = new Vector3(Random.Range(pickerArea.center.x - pickerArea.extents.x, pickerArea.center.x + pickerArea.extents.x), size/2, Random.Range(pickerArea.center.z - pickerArea.extents.z, pickerArea.center.z + pickerArea.extents.z));
-                
-                obj.transform.localScale = new Vector3(Random.Range(0.005f, size), Random.Range(0.005f, size), Random.Range(0.005f, size));
-                obj.transform.parent = spawned.transform;
-                obj.transform.localPosition = pos;
-                obj.transform.rotation = Random.rotation;
-                obj.GetComponent<Renderer>().material = Resources.Load<Material>($"Materials/Debris");
-
-                var lab = obj.AddComponent<Labeling>();
-                lab.labels.Add("debris");
-
-                var rb = obj.AddComponent<Rigidbody>();
-                rb.isKinematic = isKinematic;
             }
+            var pos = new Vector3(UnityEngine.Random.Range(-size * 5,size * 5), UnityEngine.Random.Range(0.01f, 1f), UnityEngine.Random.Range(-size * 5,size * 5));
+            
+            obj.transform.localScale = new Vector3(UnityEngine.Random.Range(0.005f, size), UnityEngine.Random.Range(0.005f, size), UnityEngine.Random.Range(0.005f, size));
+            obj.transform.parent = m_spawnedBoxes.transform;
+            obj.transform.localPosition = pos;
+            obj.transform.rotation = UnityEngine.Random.rotation;
+            obj.GetComponent<Renderer>().material = Resources.Load<Material>($"Materials/Debris");
 
-            spawned.transform.position = locationPicker.transform.position;
+            var lab = obj.AddComponent<Labeling>();
+            lab.labels.Add("debris");
+
+            var rb = obj.AddComponent<Rigidbody>();
         }
 
-        public void ShowPicker(bool show)
+        m_spawnedBoxes.transform.position = LocationPicker.transform.position;
+    }
+
+    public void ShowRobot()
+    {
+        if (!RobotInitiated)
         {
-            locationPicker.SetActive(show);
+            ImportSettings settings = ImportSettings.DefaultSettings();
+            GameObject rob = RosSharp.Urdf.Editor.UrdfRobotExtensions.Create("/Users/vidur.vij/Desktop/Robotics-AMR-Spike/Warehouse_2020.2/Assets/turtlebot3/model.urdf", settings);
+            if (rob == null)
+                Debug.Log("nope");
+            //Tile Constraint
+            GameObject tiltConstraint = new GameObject("TiltConstraint");
+            //tiltConstraint.transform.parent = rob.transform;
+            ConfigurableJoint constraint = tiltConstraint.AddComponent<ConfigurableJoint>();
+            tiltConstraint.GetComponent<Rigidbody>().isKinematic = true;
+            constraint.angularXMotion = ConfigurableJointMotion.Locked;
+            constraint.angularZMotion = ConfigurableJointMotion.Locked;
+            GameObject baseLink = GameObject.Find("base_link");
+            constraint.connectedArticulationBody = baseLink.GetComponent<ArticulationBody>();
+
+            //Remove Collisions
+            Collider[] casterCollider = GameObject.Find("caster_back_right_link").GetComponentsInChildren<Collider>();
+            foreach (Collider col in casterCollider)
+                DestroyImmediate(col);
+
+            Collider[] casterCollider2 = GameObject.Find("caster_back_left_link").GetComponentsInChildren<Collider>();
+            foreach (Collider col in casterCollider2)
+                DestroyImmediate(col);
+            //DestroyController
+            DestroyImmediate(rob.GetComponent<RosSharp.Control.Controller>());
+            //TensorUpdate
+            Vector3 inertiaup = new Vector3(1, 1, 1);
+            GameObject wheel1 = GameObject.Find("wheel_left_link").gameObject;
+            GameObject wheel2 = GameObject.Find("wheel_right_link").gameObject;
+            //DestroyImmediate(wheel1.GetComponent<JointControl>());
+            //DestroyImmediate(wheel2.GetComponent<JointControl>());
+            InertiaTensorUpdate wheel1up = wheel1.gameObject.AddComponent<InertiaTensorUpdate>();
+            InertiaTensorUpdate wheel2up = wheel2.gameObject.AddComponent<InertiaTensorUpdate>();
+            wheel1up.inertiaTensor = wheel2up.inertiaTensor = inertiaup;
+
+            //AddController
+            RosSharp.Control.Goal goal = rob.AddComponent<RosSharp.Control.Goal>();
+            RosSharp.Control.AGVController controller = rob.AddComponent<RosSharp.Control.AGVController>();
+            controller.wheel1 = wheel1;
+            controller.wheel2 = wheel2;
+            controller.goalFunc = goal;
+            controller.centerPoint = baseLink;
+            controller.forceLimit = 100;
+            controller.damping = 100;
+            controller.maxLinearSpeed = .2f;
+            controller.maxRotationalSpeed = .2f;
+            Robot = rob;
+            RobotInitiated = true;
         }
-    };
-}
+
+        Robot.transform.position = LocationPicker.transform.position;
+        Debug.Log(Robot.transform.position);
+        Debug.Log(LocationPicker.transform.position);
+    }
+
+    public void ShowPicker(){
+        LocationPicker.SetActive(true);
+    }
+
+    public void HidePicker(){
+        LocationPicker.SetActive(false);
+    }
+};
